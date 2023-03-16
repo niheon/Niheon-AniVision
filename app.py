70c9224b6617aeea05cb50ecf80d2f9285cd8c8f
niheon <niheon88@gmail.com>
2023-03-15 19:34:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-# Import necessary libraries
-import streamlit as st  # for creating the UI and handling user input
-import cv2  # for reading and processing image files
-import numpy as np  # for working with numerical data
-import tensorflow as tf  # for loading and using the pre-trained model
-from tensorflow.keras.preprocessing import image  # for preprocessing images
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input  # for using MobileNetV2 model for preprocessing
-from tensorflow.keras.applications.mobilenet_v2 import decode_predictions  # for decoding the model predictions
-from tensorflow.keras import backend as K  # for accessing the model layers and gradients
+import streamlit as st
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import decode_predictions
+from tensorflow.keras import backend as K
 
 # Set page title
 st.set_page_config(
@@ -34,44 +33,46 @@ animal_dict = {0: 'dog',
                5: 'cat',
                6: 'cow'}
 
-# Run app
+# Create session state to store prediction made
+def get_or_create_session_state():
+    if "session_state" not in st.session_state:
+        st.session_state["session_state"] = {}
+    return st.session_state["session_state"]
+
+# Run app if image file is uploaded
 if uploaded_file is not None:
 
     # Read and preprocess image
-    # Convert uploaded file to bytes
     img_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    # Decode bytes into an image array
     img = cv2.imdecode(img_bytes, 1)
-    # Convert the image array to RGB format
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # Resize the image to 224x224 pixels (the input size required by the pre-trained model)
     img_resize = cv2.resize(img, (224,224))
-    # Preprocess the resized image using MobileNetV2 preprocessing function
     img_resize = mobilenet_v2_preprocess_input(img_resize)
-    # Reshape the preprocessed image to match the input shape of the model
     img_reshape = img_resize[np.newaxis,...]
 
     # Display image
     st.image(img, channels="RGB")
 
     # Run prediction
-    # Create a button for running the prediction
-    predict_img = st.button("Predict Animal")    
+    predict_img = st.button("Predict Animal")
     if predict_img:
-        # Get the prediction probabilities for each animal class
         prediction = model.predict(img_reshape).argmax()
         try:
-            # Look up the predicted animal class from the animal dictionary
             st.title("Predicted Animal is {}".format(animal_dict[prediction]))
+            session_state = get_or_create_session_state()
+            session_state["prediction_made"] = True
         except:
-            # Display an error message if the prediction fails
             st.title("Sorry, we are unable to predict")
 
-    # Collect user feedback
-    feedback = st.selectbox("Is the predicted animal correct?", ["","Yes", "No"])
-    if feedback == "Yes":
-        st.markdown("Thank you for confirming the prediction!")
-    elif feedback == "No":
-        st.markdown("We apologize for the incorrect prediction. Please try again with a different image.")
-    else:
-        st.warning("Please provide feedback on the predicted animal.")
+    # Check if the prediction has been made before displaying the feedback selectbox
+    session_state = get_or_create_session_state()
+    if "prediction_made" in session_state and session_state["prediction_made"]:
+        feedback = st.selectbox("Is the predicted animal correct?", ["", "Yes", "No"])
+        if feedback == "Yes":
+            st.markdown("Thank you for confirming the prediction!")
+            session_state["prediction_made"] = False
+        elif feedback == "No":
+            st.markdown("We apologize for the incorrect prediction. Please try again with a different image.")
+            session_state["prediction_made"] = False
+        else:
+            st.warning("Please provide feedback on the predicted animal.")
